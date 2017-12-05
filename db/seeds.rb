f23b41e29ec9bbd77b23ae143e2373663a17c229
merly <merlysmonsalve@gmail.com>
2017-12-05 12:58:36 -0400

    primer commit

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Kindle.create({"inventory_id"=>"OSX001", "status"=>"Borrowed"})
+Kindle.create({"inventory_id"=>"OSX002", "status"=>"Free"})
+Kindle.create({"inventory_id"=>"OSX003", "status"=>"Free"})
+Kindle.create({"inventory_id"=>"OSX004", "status"=>"Borrowed"})
